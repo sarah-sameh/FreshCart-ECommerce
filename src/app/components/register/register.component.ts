4c759944f705fec9798e7ef2d78cc05ba5cfbb07
sarah-sameh <zarasamy18@gmail.com>
2024-07-29 21:59:10 +0300

    Confirm Password

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormControlOptions, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../../shared/services/authService/auth.service';
@@ -19,13 +19,36 @@ export class RegisterComponent {
 
   msgError:string='';
   isLoading:boolean=false;
+
+
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{6,20}$/)]),
-    rePassword: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{6,20}$/)]),
-    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
-  });
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^[A-Z][a-z0-9]{6,20}$/)]),
+
+
+
+    rePassword: new FormControl(''),
+
+
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  } , {validators:[this.confirmPassword]} as FormControlOptions);
+
+
+  confirmPassword( group:FormGroup ):void{
+
+    let password = group.get('password');
+    let rePassword = group.get('rePassword');
+
+   if(rePassword?.value == '')
+   {
+    rePassword?.setErrors({required:true})
+   }
+   else if(password?.value != rePassword?.value){
+    rePassword?.setErrors({mismatch:true})
+  }
+
+  }
 
   handelForm(): void {
     // console.log(this.registerForm.value);
